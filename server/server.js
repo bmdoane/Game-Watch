bfc256430ab405306ea409c0f00242242f48ad2f
Brian Doane <brianmdoane@gmail.com>
2016-10-21 13:04:19 -0500

    installed mongoose, body-parser and further built out server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 'use strict'
 
+const { json } = require('body-parser')
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/gamewatch'
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) =>
-	res.send('<h1>It works</h1>')
-)
-// app.use(express.static('client'))
+app.use(express.static('client'))
 
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+mongoose.Promise = Promise
+mongoose.connect(MONGODB_URL, () =>
+	app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+)
