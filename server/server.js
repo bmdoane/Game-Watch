697fa68a3b99596a7c4fa1c16a4316b928fa22c7
Brian Doane <brianmdoane@gmail.com>
2016-10-23 01:23:44 -0500

    generic user model and post method, creating new user via postman to mongodb

--- server/server.js
+++ server/server.js
@@ -9,6 +9,23 @@ const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/gamewa
 const PORT = process.env.PORT || 3000
 
 app.use(express.static('client'))
+app.use(json())
+
+// With model and post able to create user to mongodb via postman
+// Investigate teams being an array of objects
+const User = mongoose.model('user', {
+		username: String,
+		teams: [String]
+})
+
+app.post('/api/newUser', (req, res, err) => {
+	console.log("req", req);
+	console.log("req.body", req.body)
+	User
+		.create(req.body)
+		.then(user => res.json(user))
+		.catch(err)
+})
 
 mongoose.Promise = Promise
 mongoose.connect(MONGODB_URL, () =>
