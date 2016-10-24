8b8b294051896b8e4b548257f7408176879140c9
Brian Doane <brianmdoane@gmail.com>
2016-10-23 23:57:02 -0500

    apikey working

--- server/server.js
+++ server/server.js
@@ -3,6 +3,12 @@
 const { json } = require('body-parser')
 const express = require('express')
 const mongoose = require('mongoose')
+const request = require('request')
+
+// API key protection
+const dotenv = require('dotenv')
+dotenv.load()
+const MYSPORTSFEEDSSCHEDULE_URL = process.env.MYSPORTSFEEDSSCHEDULE_URL
 
 const app = express()
 const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/gamewatch'
@@ -19,12 +25,16 @@ const User = mongoose.model('user', {
 })
 
 app.post('/api/newUser', (req, res, err) => {
-	console.log("req", req);
 	console.log("req.body", req.body)
 	User
 		.create(req.body)
 		.then(user => res.json(user))
 		.catch(err)
+})
+
+// This logs mysports object
+request(MYSPORTSFEEDSSCHEDULE_URL, (err, res, body) => {
+	console.log("req.SPORTS", body);
 })
 
 mongoose.Promise = Promise
