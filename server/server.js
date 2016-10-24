b4e5e20d78d6e543fe2a1e32fc34be5016dcb6c9
Brian Doane <brianmdoane@gmail.com>
2016-10-24 10:53:34 -0500

    sorting delete path with postman

--- server/server.js
+++ server/server.js
@@ -32,10 +32,17 @@ app.post('/api/newUser', (req, res, err) => {
 		.catch(err)
 })
 
-// This logs mysports object
-request(MYSPORTSFEEDSSCHEDULE_URL, (err, res, body) => {
-	console.log("req.SPORTS", body);
+app.get('/api/deleteUser/:id', (req, res, err) => {
+	// Testing in Postman with - localhost:3000/api/deleteUser/580e2d78f9ba2d77549e62b9  (id of user in mongoDB)
+	console.log("req", req);
+	console.log("Dreq.body", req.params.id);
+
 })
+
+// This logs mysports object
+// request(MYSPORTSFEEDSSCHEDULE_URL, (err, res, body) => {
+// 	console.log("req.SPORTS", body);
+// })
 
 mongoose.Promise = Promise
 mongoose.connect(MONGODB_URL, () =>
