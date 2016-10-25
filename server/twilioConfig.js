af3928cf0f02e6b7d2c0f54e268573bc6d7e2ed7
Brian Doane <brianmdoane@gmail.com>
2016-10-25 01:22:42 -0500

    thinned twilioConfig.js code

--- server/twilioConfig.js
+++ server/twilioConfig.js
@@ -1,30 +1,27 @@
 'use strict'
 
-const dotenv = require('dotenv');
-let cfg = {};
+// Don't think I need these top lines
+// const dotenv = require('dotenv');
+// let cfg = {};
 
-if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') {
-  dotenv.config({path: '.env'});
-} else {
-  dotenv.config({path: '.env.test', silent: true});
+// if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') {
+//   dotenv.config({path: '.env'});
+// } else {
+//   dotenv.config({path: '.env.test', silent: true});
+// }
+
+// // HTTP Port to run our web application
+// cfg.port = process.env.PORT || 3000;
+
+// // A random string that will help generate secure one-time passwords and
+// // HTTP sessions
+// cfg.secret = process.env.APP_SECRET || 'keyboard cat';
+
+const cfg = {
+	accountSid: process.env.TWILIO_ACCOUNT_SID,
+	authToken: process.env.TWILIO_AUTH_TOKEN,
+	sendingNumber: process.env.TWILIO_NUMBER
 }
-
-// HTTP Port to run our web application
-cfg.port = process.env.PORT || 3000;
-
-// A random string that will help generate secure one-time passwords and
-// HTTP sessions
-cfg.secret = process.env.APP_SECRET || 'keyboard cat';
-
-// Your Twilio account SID and auth token, both found at:
-// https://www.twilio.com/user/account
-//
-// A good practice is to store these string values as system environment
-// variables, and load them from there as we are doing below. Alternately,
-// you could hard code these values here as strings.
-cfg.accountSid = process.env.TWILIO_ACCOUNT_SID;
-cfg.authToken = process.env.TWILIO_AUTH_TOKEN;
-cfg.sendingNumber = process.env.TWILIO_NUMBER;
 
 const requiredConfig = [cfg.accountSid, cfg.authToken, cfg.sendingNumber];
 const isConfigured = requiredConfig.every(function(configValue) {
