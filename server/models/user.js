09e4af3814d3866bafeba9fc7830057fad6fcf0c
Brian Doane <brianmdoane@gmail.com>
2016-10-27 09:17:57 -0500

    commented req: true out of schema email and password model, getting users made through postman

--- server/models/user.js
+++ server/models/user.js
@@ -5,15 +5,19 @@ const mongoose = require('mongoose')
 // Model (to be refactored) for MongoDB
 // Look at flattening game and reminder data by using userId
 module.exports = mongoose.model('User', {
-		userName: String,
+		userName: {
+			type: String,
+			required: true,
+			index: { unique: true }
+		},
 		email: {
 			type: String,
-			lowercase: true,
-			required: true
+			lowercase: true
+			// required: true
 		},
 		password: {
-			type: String,
-			required: true
+			type: String
+			// required: true
 		},
 		phoneNumber: String,
 		teamsId: [String],
