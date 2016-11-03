10256fa1eb9b0758e3519877c9f971ca498a9126
Brian Doane <brianmdoane@gmail.com>
2016-11-03 10:26:43 -0500

    team and games left in the season hitting myTeam page, along with user info

--- client/app/controllers/MyTeamCtrl.js
+++ client/app/controllers/MyTeamCtrl.js
@@ -1,11 +1,37 @@
 'use strict'
 
-app.controller('MyTeamCtrl', function($scope, UserFactory) {
+app.controller('MyTeamCtrl', function($scope, UserFactory, TeamsFactory) {
+
+
 
 	UserFactory.getUserInfo()
 	.then((userInfo) => {
 		console.log("userInfo.data", userInfo.data)
 		$scope.user = userInfo.data
+
+		UserFactory.getTeam($scope.user.teamId)
+		.then((teamInfo) => {
+			console.log("teamInfo", teamInfo)
+			$scope.team = teamInfo
+
+			$scope.urCity = ''
+			$scope.urTeam = ''
+			if ($scope.team[0].awayTeam.ID === $scope.user.teamId) {
+				$scope.urCity = $scope.team[0].awayTeam.City
+			} else if ($scope.team[0].homeTeam.ID === $scope.user.teamId) {
+				$scope.urCity = $scope.team[0].homeTeam.City
+			}
+
+			if ($scope.team[0].awayTeam.ID === $scope.user.teamId) {
+				$scope.urTeam = $scope.team[0].awayTeam.Name
+			} else if ($scope.team[0].homeTeam.ID === $scope.user.teamId) {
+				$scope.urTeam = $scope.team[0].homeTeam.Name
+			}		
+		})
 	})
+
+	$scope.today = UserFactory.getCurrentDate()
+	console.log("$scope.today", $scope.today);
+
 
 })
--- client/app/factories/UserFactory.js
+++ client/app/factories/UserFactory.js
@@ -6,6 +6,42 @@ app.factory('UserFactory', function($q, $http) {
 		return $http.get(`/api/user`)
 	}
 
-	return { getUserInfo }
+	let teamSchedule = []
+	const getTeam = (teamId) => {
+		return $q((resolve, reject) => {
+			$http.get(`/exApi/getTeamData/${teamId}`)
+			.success((teamObj) => {
+				let teamGames = teamObj
+				Object.keys(teamGames).forEach((key) => {
+					teamGames[key].id = key
+					teamSchedule.push(teamGames[key])
+				})
+				resolve(teamSchedule)
+			})
+			.error((error) => {
+				reject(error)
+			})
+		})
+	}
+
+	const getCurrentDate = () => {
+		let today = new Date();
+		let day = today.getDate();
+		let month = today.getMonth()+1; //January is 0!
+		let year = today.getFullYear();
+
+		if(day<10) {
+		    day='0'+day
+		} 
+
+		if(month<10) {
+		    month='0'+month
+		} 
+
+		today = `${year}-${month}-${day}`
+		return today
+	}
+
+	return { getUserInfo, getTeam, getCurrentDate }
 
 })
